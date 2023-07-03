26babe1dae78bfe4d7ecdcee520f8e21e8b50f01
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-07-03 23:01:06 +0200

    Create Navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='w-full py-3 px-6 flex items-center justify-between bg-slate-300 text-lg font-bold'>
+            <h1>
+                NotaGest
+            </h1>
+        </nav>
+    );
+};
+
+export default Navbar;
