4191bfd0085cd4346c2a5ab92492224aab924926
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-07-09 18:45:55 +0200

    Create featuresObject

---
+++ frontend/src/constants/featuresObject.js
@@ -0,0 +1,69 @@
+const featuresObject = [
+    {
+        id: 0,
+        text: "Prêt",
+        checked: false,
+    }, {
+        id: 1,
+        text: "Permis de constuire",
+        checked: false,
+    }, {
+        id: 2,
+        text: "Autorisation d'urbanisme",
+        checked: false,
+    }, {
+        id: 3,
+        text: "Autorisation des copropriétaires",
+        checked: false,
+    }, {
+        id: 4,
+        text: "Vente d'un bien préalable",
+        checked: false,
+    }, {
+        id: 5,
+        text: "Plus-value",
+        checked: false,
+    }, {
+        id: 6,
+        text: "Représentant fiscal",
+        checked: false,
+    }, {
+        id: 7,
+        text: "Négociation",
+        checked: false,
+    }, {
+        id: 8,
+        text: "Procuration à établir",
+        checked: false,
+    }, {
+        id: 9,
+        text: "DPU",
+        checked: false,
+    }, {
+        id: 10,
+        text: "Purge préemption locataire",
+        checked: false,
+    }, {
+        id: 11,
+        text: "Purge préemption preneur rural",
+        checked: false,
+    }, {
+        id: 12,
+        text: "Purge préemption voisin (bois)",
+        checked: false,
+    }, {
+        id: 13,
+        text: "Prêt vendeur à rembourser",
+        checked: false,
+    }, {
+        id: 14,
+        text: "Ordre irrévocable",
+        checked: false,
+    }, {
+        id: 15,
+        text: "Compromis d'agence",
+        checked: false,
+    },
+];
+
+export default featuresObject;
