f9f93ae8ded224f3f36540d899b03332d0f3120a
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-07-16 18:45:38 +0200

    Add CreateFilsForm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-import CreateFilesForm from "./components/CreateFilesForm";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="bg-white text-slate-900">
       <Navbar />
-      <CreateFilesForm />
     </div>
   );
 }
