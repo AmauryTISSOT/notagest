ccb80060616e7fcd0a04a314aefc0341a06e81e1
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-07-17 22:07:41 +0200

    Add check-circle and arrow-right-circle

--- frontend/src/assets/icons.js
+++ frontend/src/assets/icons.js
@@ -19,6 +19,14 @@ const icons = {
         <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
     ),
+    check_circle: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+    ),
+    arrow_right_circle: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+    ),
 };
 
 export default icons;
