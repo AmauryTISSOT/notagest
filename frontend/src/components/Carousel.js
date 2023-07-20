95e2a424072a282ddedfae97a570e5ff134e8dcd
amaury <tissot.amaury@gmail.com>
2023-07-20 17:56:43 +0200

    Add anchor behavior

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -5,6 +5,7 @@ const Carousel = () => {
 
     const [chevronLeft, setChevronLeft] = useState(false);
     const [chevronRight, setChevronRight] = useState(false);
+    const [divOnScreen, setDivOnScreen] = useState(Math.floor(window.innerWidth / 208));
 
     const test1 = [
         {
@@ -50,18 +51,41 @@ const Carousel = () => {
         },
     ];
 
+
     //TODO: create a carrousel to access div outside window
     // the div on the right or left should have a fading effect and an arrow to move the carousel
     // when clicking on the element div should open a windows with a form to check the element or add a date
     // create a button to create a new element
 
+    const scrollHandler = (scrollDirection) => {
+        if (scrollDirection === "right") {
+            if (test1.length > divOnScreen) {
+                console.log("right");
+                setDivOnScreen(divOnScreen + 3);
+            }
+            else {
+                setDivOnScreen(test1.length);
+            }
+        }
+
+        if (scrollDirection === "left") {
+            if (divOnScreen > 0) {
+                console.log("left");
+                setDivOnScreen(divOnScreen - 3);
+            }
+        }
+    };
+
     return (
         <div className='relative'>
-            <div className={`flex items-center ${chevronLeft && 'translate-x-[200px] duration-100'} ${chevronRight && 'translate-x-[-200px] duration-100'}`}>
+            <div className='flex items-center overflow-scroll scroll-smooth'
+                id='scrollElement'
+            >
                 {test1.map((item, index) => {
                     return (
-                        <div className='bg-slate-200 flex flex-col p-1 m-1 w-[200px] rounded last:bg-gradient-to-r from-slate-200 to-white'
-                            key={index}>
+                        <div className='bg-slate-200 flex flex-col p-1 m-1 rounded min-w-[200px]'
+                            key={index}
+                            id={index}>
                             {item.status ?
                                 <span>{icons.check_circle}</span> :
                                 <span>{icons.arrow_right_circle}</span>}
@@ -71,8 +95,15 @@ const Carousel = () => {
                     );
                 })}
             </div>
-            <span className='absolute left-0 top-0 z-10 bg-slate-50 h-full' onClick={() => setChevronLeft(!chevronLeft)}>{icons.chevron_left}</span>
-            <span className='absolute right-0 top-0 z-10 bg-slate-50 h-full' onClick={() => setChevronRight(!chevronRight)}>{icons.chevron_right}</span>
+            {
+                chevronRight &&
+                <span className='absolute left-0 top-0 z-10 h-full flex justify-start items-center w-10 rounded bg-gradient-to-r from-white cursor-pointer' onClick={() => setChevronLeft(!chevronLeft)}>{icons.chevron_left}</span>
+            }
+            <a href={`#${divOnScreen}`} onClick={() => scrollHandler("right")}>
+
+                <span className='absolute right-0 top-0 z-10 h-full flex justify-start items-center w-10 rounded bg-gradient-to-l from-white cursor-pointer' onClick={() => setChevronRight(!chevronRight)}>{icons.chevron_right}</span>
+            </a>
+            {console.log('div', divOnScreen)}
         </div>
     );
 };
